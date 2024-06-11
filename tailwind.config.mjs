15296d15726939d117719d7a81bc9d0127cf321e
leoferreiralima <leoferreiralima@yahoo.com>
2024-06-11 09:59:21 -0300

    feat: add header and logo svg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,13 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+			serif: defaultTheme.fontFamily.serif
+		},
 		extend: {},
 	},
 	plugins: [],
